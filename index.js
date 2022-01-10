2c3fb643ab0c6858c1fbfd16fcd94e140555b2b5
uae <aniekaudo@yahoo.com>
2022-01-10 11:08:41 +0100

    Added luxon date and time

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import { form, navs, title, author } from './modules/pageElements.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+import {
+  form,
+  navs,
+  title,
+  author,
+  todayDate,
+} from './modules/pageElements.js';
 import { findHight } from './modules/manupulateDom.js';
-import Books from "./modules/Books.js";
+import Books from './modules/Books.js';
 
 (() => {
   Books.load();
   findHight();
+  todayDate.textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (title.value !== '' && author.value !== '') {
@@ -16,18 +24,24 @@ import Books from "./modules/Books.js";
       Books.showAlert('Please fill all the fields', 'danger');
     }
   });
-  
+
   document.querySelectorAll('.removeBook').forEach((deleteBook) => {
     deleteBook.addEventListener('click', () => Books.removeBook(deleteBook));
   });
-  
+
   navs.forEach((nav) => {
     nav.addEventListener('click', (e) => {
-      document.querySelectorAll('.nav').forEach((nav2) => nav2.classList.remove('activelink'));
+      document
+        .querySelectorAll('.nav')
+        .forEach((nav2) => nav2.classList.remove('activelink'));
       e.preventDefault();
-      document.querySelectorAll('.tab').forEach((tab) => tab.classList.remove('active'));
+      document
+        .querySelectorAll('.tab')
+        .forEach((tab) => tab.classList.remove('active'));
       e.target.classList.add('activelink');
-      document.querySelector(e.target.getAttribute('href')).classList.add('active');
+      document
+        .querySelector(e.target.getAttribute('href'))
+        .classList.add('active');
       findHight();
     });
   });
